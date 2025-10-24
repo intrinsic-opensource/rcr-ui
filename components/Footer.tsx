ea26241675dd110f5a25eb3ac502d889713478af
Andrew Symington <simmers@intrinsic.ai>
2025-10-24 23:40:46 +0000

    Rephrase a few things on the website.
    Signed-off-by: Andrew Symington <simmers@intrinsic.ai>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 interface FooterProps {}
 
-export const REPORT_LINK =
-  'https://github.com/intrinsic-opensource/ros-central-registry/tree/main/docs#requesting-to-take-down-a-module'
-export const BCR_UI_REPO_LINK = 'https://github.com/bazel-contrib/bcr-ui'
+export const REPORT_LINK = 'https://github.com/intrinsic-opensource/rcr-ui/issues'
+export const RCR_UI_REPO_LINK = 'https://github.com/intrinsic-opensource/rcr-ui'
 
 export const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className="bg-ros-blue flex flex-col items-center justify-center gap-2 h-18 mt-4 py-2.5">
       <div className="text-center text-white">
-        The ROS Central Registry is maintained by the ROS PMC.
+        Open source robotics tooling, powered by Intrinsic and the OSRF.
       </div>
       <div className="text-center text-white">
         To report an issue with one of the modules, see the{' '}
@@ -25,7 +24,7 @@ export const Footer: React.FC<FooterProps> = () => {
       <div className="text-center text-white">
         The source of this website can be found in{' '}
         <a
-          href={BCR_UI_REPO_LINK}
+          href={RCR_UI_REPO_LINK}
           className="text-white hover:text-gray-300"
         >
           this repository
